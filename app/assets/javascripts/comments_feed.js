1e1b141d33b9d07bd87f9fa9af56260fb921dd76
Thomas Abend <tsabend@gmail.com>
2014-11-04 18:21:24 -0500

    Refactored javascript

--- app/assets/javascripts/comments_feed.js
+++ app/assets/javascripts/comments_feed.js
@@ -1,39 +1,27 @@
-// $(document).on('page:change', function() {
-// 	if( $('[data-card=commentsfeed]').length > 0) {
-// 		var id = ajaxInterval('commentsfeed')
-// 	}
-// 	else {
-// 		clearInterval(id)
-// 	}
-// 	if( $('[data-card=mostcommented]').length > 0) {
-// 		var id2 = ajaxInterval('mostcommented')
-// 	}
-// 	else {
-// 		clearInterval(id2)
-// 	}
-// 	if( $('[data-card=activeusers]').length > 0) {
-// 		var id3 = ajaxInterval('activeusers')
-// 	}
-// 	else {
-// 		clearInterval(id3)
-// 	}
-// })
+$(document).on('page:change', function() {
+	pollCards()
+})
 
-// // Data card should be the URL and value of the data-card attribute
-// function ajaxInterval(dataCard) {
-// 	var storyId = $("[data-card='" + dataCard + "']").data('story')
-// 	return setInterval(function(){
-// 		fetchPartial(dataCard, storyId, function(template){
-// 			$("[data-card='" + dataCard + "']").replaceWith(template)
-// 			openComment()
-// 		})
-// 	}, 10000)
-// }
+// Data card should be the URL and value of the data-card attribute
+function pollCards() {
+	updateCards()
+	setTimeout(pollCards, 10000)
+}
 
+function updateCards() {
+	var cards = $("[data-card]")
+	for(var i = 0; i < cards.length; i++) {
+		var storyId = cards[i].dataset.story
+		fetchPartial(cards[i].dataset.card, storyId, function(card, template){
+			$(card).replaceWith(template)
+			openComment()
+		}.bind(this, cards[i]))
+	}
+}
 
-// function fetchPartial(url, id, callback) {
-// 	$.ajax({
-// 	    url: '/stories/' + id + '/' + url,
-// 	  })
-// 	  .success(callback)
-// }
+function fetchPartial(url, id, callback) {
+	$.ajax({
+	    url: '/stories/' + id + '/' + url,
+	  })
+	  .success(callback)
+}
