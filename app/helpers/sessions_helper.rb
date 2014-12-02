7152b33ee49ed381f113f866ea1cde7117340489
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-12-01 18:48:55 -0600

    added splash.css to config

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def current_user
     # @current_user ||= User.find_by(id: session[:user_id])
-    @current_user ||= User.find(1)
+    # @current_user ||= User.find(1)
   end
 
   def user_logged_in?
