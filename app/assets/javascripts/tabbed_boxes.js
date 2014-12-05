e60faa3d0a037d577c78cdf0a404476d38ceedc0
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-12-05 09:02:44 -0600

    story page feeds updated

--- app/assets/javascripts/tabbed_boxes.js
+++ app/assets/javascripts/tabbed_boxes.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {	
+$(document).on('page:change', function() {
 	$(".stories_link").on('click',function(e){
 		e.preventDefault();
 		var localId = $(this).data("id") 
 		$("ul[data-id="+localId+"] li").removeClass("pure-menu-selected")
 		$(this).addClass("pure-menu-selected")
-		// var html_replacement =  $(".stories_for_class[data-id="+localId+"]")
 		current_card = document.getElementById("class_info"+localId+"")
 		current_card.children[0].style.display = "none"
 		current_card.children[1].style.display = "block"
@@ -16,7 +15,6 @@ $(document).ready(function() {
 		var localId = $(this).data("id") 
 		$("ul[data-id="+localId+"] li").removeClass("pure-menu-selected")
 		$(this).addClass("pure-menu-selected")
-		// var html_replacement =  $(".stories_for_class[data-id="+localId+"]")
 		current_card = document.getElementById("class_info"+localId+"")
 		current_card.children[0].style.display = "block"
 		current_card.children[1].style.display = "none"
@@ -27,13 +25,40 @@ $(document).ready(function() {
 		var localId = $(this).data("id") 
 		$("ul[data-id="+localId+"] li").removeClass("pure-menu-selected")
 		$(this).addClass("pure-menu-selected")
-		// var html_replacement =  $(".stories_for_class[data-id="+localId+"]")
+		current_card = document.getElementById("class_info"+localId+"")
+		current_card.children[0].style.display = "none"
+		current_card.children[1].style.display = "none"
+		current_card.children[2].style.display = "block"
+	})
+		$(".commented_sentences_link").on('click',function(e){
+		e.preventDefault();
+		var localId = $(this).data("id") 
+		$("ul[data-id="+localId+"] li").removeClass("pure-menu-selected")
+		$(this).addClass("pure-menu-selected")
+		current_card = document.getElementById("class_info"+localId+"")
+		current_card.children[0].style.display = "none"
+		current_card.children[1].style.display = "block"
+		current_card.children[2].style.display = "none"
+	})
+		$(".link_to_recent_comments").on('click',function(e){
+		e.preventDefault();
+		var localId = $(this).data("id") 
+		$("ul[data-id="+localId+"] li").removeClass("pure-menu-selected")
+		$(this).addClass("pure-menu-selected")
+		current_card = document.getElementById("class_info"+localId+"")
+		current_card.children[0].style.display = "block"
+		current_card.children[1].style.display = "none"
+		current_card.children[2].style.display = "none"
+	})
+		$(".frequent_commentors_link").on('click',function(e){
+		e.preventDefault();
+		var localId = $(this).data("id") 
+		$("ul[data-id="+localId+"] li").removeClass("pure-menu-selected")
+		$(this).addClass("pure-menu-selected")
 		current_card = document.getElementById("class_info"+localId+"")
 		current_card.children[0].style.display = "none"
 		current_card.children[1].style.display = "none"
 		current_card.children[2].style.display = "block"
 	})
 
-	
-
-});
+	});
