ed79eb5175cf1728a2d8ba3fb807192ea851cdd9
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-12-15 12:57:08 -0600

    moved yui script to notification_popup.js

--- app/assets/javascripts/notification_popup.js
+++ app/assets/javascripts/notification_popup.js
@@ -1,4 +1,21 @@
 $(document).on('page:change', function() {
+  YUI({
+
+    classNamePrefix: 'pure'
+  }).use('gallery-sm-menu', function (Y) {
+
+    var horizontalMenu = new Y.Menu({
+      container         : '.pure-menu',
+      sourceNode        : '#std-menu-items',
+      orientation       : 'horizontal',
+      hideOnOutsideClick: false,
+      hideOnClick       : false
+    });
+
+    horizontalMenu.render();
+    horizontalMenu.show();
+
+  });
   $(".pure-menu-children").on("hover", function(e) {
     if (e.type == "mouseenter") {
       console.log("one");   
