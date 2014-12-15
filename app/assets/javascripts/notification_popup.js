6c902f6117f1136bc081ed4cf18e1b713f6415ce
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-12-15 01:25:04 -0600

    tabbed boxes have been refactored and are functional. plan to revisit.

--- app/assets/javascripts/notification_popup.js
+++ app/assets/javascripts/notification_popup.js
@@ -1,14 +1,13 @@
 $(document).on('page:change', function() {
   $(".pure-menu-children").on('click', function() {
     console.log($(this))
-    console.log("fire")
     $(".dropdown-menu").toggle()
     $.ajax({
       url: '/notifications',
       type: 'POST',
     })
     .success(function() {
-      console.log("success");
+      
     });
   })
 });
