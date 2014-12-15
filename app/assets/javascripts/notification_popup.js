c550cd2722b350ed20381b48fef26112cf6c1bbf
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-12-15 14:09:27 -0600

    light refactor

--- app/assets/javascripts/notification_popup.js
+++ app/assets/javascripts/notification_popup.js
@@ -1,24 +1,25 @@
 $(document).on('page:change', function() {
-  YUI({
+// pure-menu pure-menu-open pure-menu-horizontal
 
-    classNamePrefix: 'pure'
-  }).use('gallery-sm-menu', function (Y) {
+YUI({
+  classNamePrefix: 'pure'
+}).use('gallery-sm-menu', function (Y) {
 
-    var horizontalMenu = new Y.Menu({
-      container         : '.pure-menu',
-      sourceNode        : '#std-menu-items',
-      orientation       : 'horizontal',
-      hideOnOutsideClick: false,
-      hideOnClick       : false
-    });
-
-    horizontalMenu.render();
-    horizontalMenu.show();
-
+  var horizontalMenu = new Y.Menu({
+    container         : '.pure-menu',
+    sourceNode        : '#pure-menu-items',
+    orientation       : 'horizontal',
+    hideOnOutsideClick: false,
+    hideOnClick       : false
   });
-  $(".pure-menu-children").on("hover", function(e) {
-    if (e.type == "mouseenter") {
-      console.log("one");   
+
+  horizontalMenu.render();
+  horizontalMenu.show();
+
+});
+$(".pure-menu-children").on("hover", function(e) {
+  if (e.type == "mouseenter") {
+    console.log("one");   
       // $.ajax({
       //   url: '/notifications',
       //   type: 'POST',
@@ -26,9 +27,20 @@ $(document).on('page:change', function() {
       // .success(function() {
 
       // });
-    }
-    else { 
-      console.log("two");   
-    }
-  });
+}
+else { 
+  console.log("two");   
+}
+});
 })
+
+var getStatus = function(){
+  var pureMenuItem = document.getElementsByClassName("pure-menu pure-menu-open pure-menu-horizontal")
+  var statusItem = pureMenuItem.item()
+  if (statusItem.dataset.statusItem == "true") {
+    return true    
+  } else {
+    return false  
+  }
+
+}
